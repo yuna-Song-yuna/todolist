3ec972dcabb15adc838f019ac82a8173a4e508d9
yuna-Song-yuna <68729868+yuna-Song-yuna@users.noreply.github.com>
2021-02-19 14:45:56 +0900

    css width 수정 테스트

--- bgimage.js
+++ bgimage.js
@@ -6,6 +6,7 @@ function paintImage(){
     // image.src = `images/${imgNumber}.png`
     image.classList.add("bgImage")
     image.src="https://source.unsplash.com/user/erondu/1600x900"
+    // image.width="100%"
     body.prepend(image);    //prepend: body앞에 코드 추가
 }
 
